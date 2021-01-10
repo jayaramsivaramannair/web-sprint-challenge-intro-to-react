075aa7fcb4351d3c129f8aba25a80546bb0526f9
jayaramsivaramannair <jayaramsivaramannair@gmail.com>
2021-01-10 10:22:17 -0500

    Requested and Tested Data for 10 Characters from Rick and Morty API

--- src/App.js
+++ src/App.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import axios from 'axios';
+import Character from './components/Character'
+import styled from 'styled-components';
 
 const App = () => {
+  const [characters, setCharacters] = useState([]);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  //The below api call is made only on initial website load
+
+  useEffect(() => {
+      axios.get("https://rickandmortyapi.com/api/character/11,12,13,14,15,16,17,18,19,20")
+      .then((response) => {console.log(response.data); setCharacters(response.data)})
+      .catch((error) => console.log(error));
+  }, [])
+
 
   return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
+    <AppContainer className="App">
+      <Header className="Header">Rick and Morty's Universe</Header>
+      {characters.map((character) => {
+        return (
+        <Character key={`key${character.id}`} name={character.name} avatar={character.image} gender={character.gender} species={character.species} status={character.status} location={character.location.name}/>     
+      )})} 
+    </AppContainer>
   );
-}
+};
+
+const Header = styled.h1`
+  color: #C0E218;
+`;
+
+const AppContainer = styled.div`
+  font-family: 'Dokdo', cursive;
+`;
 
 export default App;
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,2 +1,32 @@
 // Write your Character component here
 //Chacters from Rick and Morty's Universe
+import React from 'react';
+import {CardImg, CardText} from 'reactstrap';
+import styled from 'styled-components';
+
+const Character = ({name, avatar, gender, species, status, location}) => {
+    return (
+        <CardContainer>
+            <RoundedImage src={avatar}/>
+            <CardText>Name: {name}</CardText>
+            <CardText>Gender: {gender}</CardText>
+            <CardText>Species {species}</CardText>
+            <CardText>Status: {status}</CardText>
+            <CardText>Location: {location}</CardText>
+        </CardContainer>
+
+    );
+};
+
+export default Character;
+
+const RoundedImage = styled.img`
+    width: 100px;
+    height: auto;
+    border-radius: 50px;
+`;
+
+const CardContainer = styled.div`
+    border: 2px solid green;
+`
+
